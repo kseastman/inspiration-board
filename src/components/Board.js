4f62d135a62d416b3e237185f57ef6fa03e97f3e
kseastman <kat.eastman@gmail.com>
2018-06-13 18:51:30 -0700

    recreating functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -33,19 +33,84 @@ class Board extends Component {
     })
   }
 
+  addCard = (card) => {
+    const BASE_URL = this.props.url
+    const BOARD = this.props.boardName
+    const CARD = `${BASE_URL + BOARD}/cards?emoji=${card.emoji}&text=${card.text}`
+
+    axios.post(CARD)
+    .then((response) => {
+      console.log(response.data)
+      let updateState = {};
+      updateState = this.state;
+
+      const newCard = response.data;
+      updateState.cards.push(newCard);
+
+      this.setState(updateState);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  }
+
+  removeCard = (card) => {
+    const BASE_URL = this.props.url
+    const BOARD = this.props.boardName
+    const CARD = `${BASE_URL + BOARD}/cards/${card.id}`
+
+    axios.delete(CARD)
+    .then((response) => {
+      console.log(response.data);
+      let updateState = {}
+      updateState = this.state;
+
+      const cardIndex = card.index;
+      updateState.cards.splice(cardIndex, 1);
+
+      this.setState(updateState);
+
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  // renderItemOrEditField( item, index ) {
+  //   if ( this.state.editing === item.id ) {
+  //     return <UpdateCardForm key={`editing-${index}`} card={item} index={index} updateCardCallback={this.updateCard}/>
+  //   } else {
+  //     return <Card
+  //       key={item.id}
+  //       id={item.id}
+  //       index={index}
+  //       emoji={item.emoji}
+  //       text={item.text}
+  //       removeCardCallback={this.removeCard}
+  //       />
+  //   }
+  // }
+
   render() {
-    const cardCollection = this.state.cards.map((post) => {
+    const cardCollection = this.state.cards.map((post, index) => {
           return <Card
             key={post.card.id}
+            id={post.card.id}
+            index={index}
             emoji={post.card.emoji}
             text={post.card.text}
+            removeCardCallback={this.removeCard}
             />
         })
 
     return (
-      <div className="board">
+      <section className="board">
         {cardCollection}
-      </div>
+        <article>
+          <NewCardForm addCardCallback={this.addCard}/>
+        </article>
+      </section>
     )
   }
 
